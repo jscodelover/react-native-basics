dc3c63c66e72ec9970eb585feb80a3e18daec33a
manisha1896 <manisha.4918@gmail.com>
2019-03-18 16:47:37 +0530

    feat: react-navigation

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 // @flow
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 import StatePropsExample from "./app/StatePropsExample";
 import ListDisplay from "./app/ListDisplay";
+import DetailDisplay from "./app/DetailDisplay";
 
-export default class App extends Component {
-  render() {
-    // return <StatePropsExample message="My Counter App" />;
-    return <ListDisplay />;
+const AppNavigator = createStackNavigator({
+  DetailDisplay: {
+    screen: DetailDisplay
   }
-}
+});
+
+export default createAppContainer(AppNavigator);
+
+// export default class App extends Component {
+//   render() {
+//     // return <StatePropsExample message="My Counter App" />;
+//     // return <ListDisplay />;
+//     return <DetailDisplay />;
+//   }
+// }
---
+++ app/DetailDisplay.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default class DetailScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        {/* <Text>{this.props.data.name}</Text>
+        <Text>{this.props.data.username}</Text>
+        <Text>{this.props.data.email}</Text>
+        <Text>{this.props.website}</Text> */}
+        <Text>Text</Text>
+      </View>
+    );
+  }
+}
